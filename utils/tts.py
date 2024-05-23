53c4dcaa41221be8d8994e7c4d293028d14be8ce
LuluBeatson <lulu.beatson@gmail.com>
2024-05-24 00:36:16 +0100

    tts speed option

--- utils/tts.py
+++ utils/tts.py
@@ -10,24 +10,65 @@ dotenv.load_dotenv()
 
 
 @click.command()
-@click.option("--model", default="tts-1", help="The model to use for the TTS")
-@click.option("--voice", default="alloy", help="The voice to use for the TTS")
+@click.option(
+    "-m", "--model", default="tts-1", help="The model, one of: tts-1, tts-1-hd"
+)
+@click.option(
+    "-v",
+    "--voice",
+    default="alloy",
+    help="A voice, one of: alloy, echo, fable, onyx, nova, shimmer",
+)
+@click.option(
+    "-s",
+    "--speed",
+    default=1.0,
+    help="The speed of the generated audio. A value between 0.25 and 4.0",
+)
+@click.option(
+    "-o",
+    "--output-file",
+    default=None,
+    help="Save the audio to a file. Allowed extensions: mp3, flac, opus, acc",
+)
 @click.argument("input_text")
-def tts(input_text, model="tts-1", voice="alloy"):
+def tts(input_text, model="tts-1", voice="alloy", speed=1.0, output_file=None):
+    """Text to speech using OpenAI's API
+
+    https://platform.openai.com/docs/guides/text-to-speech
+    """
     url = "https://api.openai.com/v1/audio/speech"
     headers = {
         "Authorization": f'Bearer {os.getenv("OPENAI_API_KEY")}',
     }
 
+    # if output_file, infer response_format from file extension
+    if output_file:
+        response_format = output_file.split(".")[-1]
+        assert response_format in ["mp3", "flac", "opus", "acc"]
+    else:
+        response_format = "opus"  # needed for realtime playback
+
     data = {
         "model": model,
         "input": input_text,
         "voice": voice,
-        "response_format": "opus",
+        "speed": speed,
+        "response_format": response_format,
     }
 
-    audio = pyaudio.PyAudio()
+    # If an output_file is specified, make a non-streaming request and save the
+    # response to the file. Otherwise, stream the response to pyaudio for realtime
+    if output_file:
+        with requests.post(url, headers=headers, json=data) as response:
+            if response.status_code == 200:
+                with open(output_file, "wb") as file:
+                    file.write(response.content)
+            else:
+                print(f"Error: {response.status_code} - {response.text}")
+        return
 
+    audio = pyaudio.PyAudio()
     with requests.post(url, headers=headers, json=data, stream=True) as response:
         if response.status_code == 200:
             buffer = io.BytesIO()
@@ -36,23 +77,27 @@ def tts(input_text, model="tts-1", voice="alloy"):
 
             buffer.seek(0)
 
-            with sf.SoundFile(buffer, "r") as sound_file:
-                format = pyaudio.paInt16
-                channels = sound_file.channels
-                rate = sound_file.samplerate
+            if output_file:
+                with open(output_file, "wb") as file:
+                    file.write(buffer.read())
+            else:
+                with sf.SoundFile(buffer, "r") as sound_file:
+                    format = pyaudio.paInt16
+                    channels = sound_file.channels
+                    rate = sound_file.samplerate
 
-                stream = audio.open(
-                    format=format, channels=channels, rate=rate, output=True
-                )
-                chunk_size = 1024
-                data = sound_file.read(chunk_size, dtype="int16")
-
-                while len(data) > 0:
-                    stream.write(data.tobytes())
+                    stream = audio.open(
+                        format=format, channels=channels, rate=rate, output=True
+                    )
+                    chunk_size = 1024
                     data = sound_file.read(chunk_size, dtype="int16")
 
-                stream.stop_stream()
-                stream.close()
+                    while len(data) > 0:
+                        stream.write(data.tobytes())
+                        data = sound_file.read(chunk_size, dtype="int16")
+
+                    stream.stop_stream()
+                    stream.close()
         else:
             print(f"Error: {response.status_code} - {response.text}")
 
